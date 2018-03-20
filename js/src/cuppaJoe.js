8970f729ea72cc813ea068d5b3389c7f0895367e
Camden_Shaw <camden.shaw@gmail.com>
2018-03-19 19:42:00 -0700

    Single page carousel and iframes within it are functioning.

--- js/src/cuppaJoe.js
+++ js/src/cuppaJoe.js
@@ -1,13 +1,8 @@
 $(document).ready(() => {
     const forceBox = () => {
-        console.log("forcebox")
         let image = $("img")
         let i = 0
-        console.log(image)
         do {
-            console.log(image[i], i)
-            console.log(image[i].height)
-            console.log($(image[i]).css("height"))
             if (
                 image[i + 1].height < image[i].height ||
                 image[i].height < image[i + 1].height
@@ -20,7 +15,6 @@ $(document).ready(() => {
             i = i + 2
         } while (i < image.length - 1)
     }
-    console.log("cuppaJoe.js is loaded with jQuery")
     window.addEventListener("resize", () => {
         forceBox()
     })
