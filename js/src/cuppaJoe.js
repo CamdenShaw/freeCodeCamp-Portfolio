deb04c6fcdccf9d0f7e5a0cef478d37d9da9e65f
Camden_Shaw <camden.shaw@gmail.com>
2018-03-27 10:58:33 -0700

    Fixed loading speed issue with new images.

--- js/src/cuppaJoe.js
+++ js/src/cuppaJoe.js
@@ -1,22 +1,26 @@
 $(document).ready(() => {
     const forceBox = () => {
+        console.log("forcebox")
         let image = $("img")
         let i = 0
+        console.log(image)
         do {
-            if (
-                image[i + 1].height < image[i].height ||
-                image[i].height < image[i + 1].height
-            ) {
+            console.log(image[i], i)
+            console.log(image[i].height)
+            console.log($(image[i]).css("height"))
+            if (image[i].height > image[i].width) {
                 $(image[i]).css("max-height", image[i + 1].height - 2)
                 $(image[i]).css("width", "auto")
-                $(image[i + 1]).css("height", `${$(image[i]).css("auto")}`)
+                if (i % 2 === 0)
+                    {$(image[i + 1]).css("height", `${$(image[i]).css("auto")}`)}
                 $(image[i + 1]).css("width", `100%`)
             }
-            i = i + 2
+            i++
         } while (i < image.length - 1)
     }
+    console.log("cuppaJoe.js is loaded with jQuery")
     window.addEventListener("resize", () => {
         forceBox()
     })
-    forceBox()
+    $(window).on("load", forceBox())
 })
