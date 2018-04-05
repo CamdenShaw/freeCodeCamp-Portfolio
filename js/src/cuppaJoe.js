39fb70f49339bffcc4a9de4318cac8de1b92cc79
Camden_Shaw <camden.shaw@gmail.com>
2018-04-05 13:01:40 -0700

    Added angular2 github page to web apps carousel, added mouseover and mouseleave events for iframes to stop and start carousels.

--- js/src/cuppaJoe.js
+++ js/src/cuppaJoe.js
@@ -1,24 +1,33 @@
 $(document).ready(() => {
+    $("iframe")
+        .mouseenter(() => {
+            $(".carousel").carousel({
+                pause: true,
+                interval: false
+            })
+        })
+        .mouseleave(() => {
+            $(".carousel").carousel({
+                pause: false,
+                interval: true
+            })
+        })
+
     const forceBox = () => {
-        console.log("forcebox")
         let image = $("img")
         let i = 0
-        console.log(image)
         do {
-            console.log(image[i], i)
-            console.log(image[i].height)
-            console.log($(image[i]).css("height"))
             if (image[i].height > image[i].width) {
                 $(image[i]).css("max-height", image[i + 1].height - 2)
                 $(image[i]).css("width", "auto")
-                if (i % 2 === 0)
-                    {$(image[i + 1]).css("height", `${$(image[i]).css("auto")}`)}
+                if (i % 2 === 0) {
+                    $(image[i + 1]).css("height", `${$(image[i]).css("auto")}`)
+                }
                 $(image[i + 1]).css("width", `100%`)
             }
             i++
         } while (i < image.length - 1)
     }
-    console.log("cuppaJoe.js is loaded with jQuery")
     window.addEventListener("resize", () => {
         forceBox()
     })
