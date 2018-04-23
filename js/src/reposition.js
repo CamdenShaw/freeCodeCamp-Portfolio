96d02bed0c8a6f9e92333ddcbe3045109364047a
Camden_Shaw <camden.shaw@gmail.com>
2018-04-23 11:03:29 -0700

    Modified styles.

--- js/src/reposition.js
+++ js/src/reposition.js
@@ -4,9 +4,9 @@ $(document).ready(() => {
 
     const reposition = () => {
         let posPercent = 100,
-            newPosition = mainSite[0].scrollTop * 0.3,
-            newPosPercent = newPosition / totalContentHeight * 100
-        mainSite.css("background-position", `0 ${newPosPercent - posPercent}%`)
+            newPosition = mainSite[0].scrollTop * 10,
+            newPosPercent = newPosition / totalContentHeight * -100
+        mainSite.css("background-position", `0 ${newPosPercent - posPercent}px`)
     }
 
     mainSite.on("scroll", () => {
