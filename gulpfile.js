42961a54462ec0c75f140f5a5e820c821bb6a457
Camden_Shaw <camden.shaw@gmail.com>
2022-02-21 17:25:38 -0800

    Changes to gulp file and npm packages to get Gulp running properly.

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const gulp = require("gulp"),
     rename = require("gulp-rename"),
     browserSync = require("browser-sync").create(),
     lintCheck = require("gulp-eslint"),
-    sass = require("gulp-sass"),
+    sass = require("gulp-sass")(require("sass")),
     autoprefixer = require("gulp-autoprefixer"),
     prettyError = require("gulp-prettyerror"),
     cssnano = require("gulp-cssnano"),
@@ -15,13 +15,23 @@ const input = "./js/src/*.js",
     css_input = "./styles/*.css",
     sass_input = "./styles/sass/*.scss",
     css_output = "./build/css",
-    sass_output = "./styles"
+    sass_output = "./styles",
+    fcc_styles = "./fcc/**/*",
+    fcc_scripts = "./fcc/**/*.js",
+    fcc_output = "."
 
 gulp.task("babel", async () => {
     return gulp
         .src(input)
         .pipe(babel())
         .pipe(gulp.dest(output))
+})
+
+gulp.task("fcc_babel", async () => {
+    return gulp
+        .src(input)
+        .pipe(babel())
+        .pipe(gulp.dest(fcc_scripts))
 })
 
 gulp.task("lint", async () => {
@@ -31,7 +41,20 @@ gulp.task("lint", async () => {
         .pipe(lintCheck.format())
         .pipe(lintCheck.failAfterError())
         .on('error', function(err) {
-            console.log(err)
+            console.error(err)
+
+            this.emit('end')
+        })
+})
+
+gulp.task("fcc_lint", async () => {
+    return gulp
+        .src(fcc_scripts)
+        .pipe(lintCheck())
+        .pipe(lintCheck.format())
+        .pipe(lintCheck.failAfterError())
+        .on('error', function(err) {
+            console.error(err)
 
             this.emit('end')
         })
@@ -65,14 +88,46 @@ gulp.task("scripts", gulp.series("lint", async () => {
         .pipe(gulp.dest(output))
 }))
 
+gulp.task("fcc_scripts", gulp.series("fcc_lint", async () => {
+    console.log("fcc scripts is running")
+    return gulp
+        .src(fcc_scripts, {base: "./"})
+        .pipe(changed(fcc_output))
+        .pipe(
+            babel({
+                presets: ["@babel/preset-env"]
+            })
+        )
+        .on("error", function(err) {
+            console.log(err)
+
+            this.emit('end')
+        })
+        .pipe(uglify())
+        .on("error", function(err) {
+            console.log(err)
+
+            this.emit('end')
+        })
+        .pipe(
+            rename({
+                extname: ".min.js"
+            })
+        )
+        .pipe(gulp.dest(fcc_output))
+}))
+
 gulp.task("hi", () => {
     console.log("Hello World!")
 })
 
 gulp.task("watch", async () => {
     gulp.watch("./js/src/*.js", gulp.parallel("scripts"))
+    gulp.watch("./fcc/**/*.js", gulp.parallel("fcc_scripts"))
     gulp.watch("./styles/sass/*.scss", gulp.parallel("sass"))
+    gulp.watch("./fcc/**/*.scss", gulp.parallel("fcc_scss"))
     gulp.watch("./styles/*.css", gulp.parallel("css"))
+    gulp.watch("./fcc/**/*.css", gulp.parallel("fcc_css"))
 })
 
 gulp.task("css", async () => {
@@ -82,9 +137,7 @@ gulp.task("css", async () => {
         .pipe(prettyError())
         .pipe(sass())
         .pipe(
-            autoprefixer({
-                browsers: ["last 2 versions"]
-            })
+            autoprefixer()
         )
         .pipe(gulp.dest(css_output))
         .pipe(cssnano())
@@ -95,6 +148,24 @@ gulp.task("css", async () => {
         .pipe(gulp.dest(css_output))
 })
 
+gulp.task("fcc_css", async () => {
+    return gulp
+        .src(fcc_styles + ".css", {base: "./"})
+        .pipe(changed(css_output))
+        .pipe(prettyError())
+        .pipe(sass())
+        .pipe(
+            autoprefixer()
+        )
+        .pipe(gulp.dest(fcc_output))
+        .pipe(cssnano())
+        .pipe(rename({
+                extname: ".min.css"
+            })
+        )
+        .pipe(gulp.dest(fcc_output))
+})
+
 gulp.task("sass", async () => {
     return gulp
         .src(sass_input)
@@ -102,20 +173,28 @@ gulp.task("sass", async () => {
         .pipe(prettyError())
         .pipe(sass())
         .pipe(
-            autoprefixer({
-                browsers: ["last 2 versions"]
-            })
+            autoprefixer()
         )
         .pipe(gulp.dest(sass_output))
+})
+
+gulp.task("fcc_scss", async () => {
+    return gulp
+        .src(fcc_styles + ".scss",{base: "./"})
+        .pipe(changed(fcc_output))
+        .pipe(prettyError())
+        .pipe(sass())
+        .pipe(
+            autoprefixer()
+        )
+        .pipe(gulp.dest(fcc_output))
 })
 
 gulp.task("sass_test", async () => {
     return gulp
         .src(css_input)
         .pipe(
-            autoprefixer({
-                browsers: ["last 2 versions"]
-            })
+            autoprefixer()
         )
         .pipe(sass())
         .on("error", err => {
@@ -136,7 +215,7 @@ gulp.task("browser-sync", async () => {
         }
     })
     return gulp
-        .watch(["build/js/*.js", "build/css/*.min.css", "*.html"])
+        .watch(["build/js/*.js", "build/css/*.min.css", "*.html", "fcc/**/*.min.css", "fcc/**/*.min.js"])
         .on("change", browserSync.reload)
     //gulp.watch('./styles/*.css').on('change', browserSync.reload)
 })
