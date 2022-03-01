03687f2f92384052d46411f3a04e49bb82008919
Camden_Shaw <camden.shaw@gmail.com>
2022-02-28 22:18:23 -0800

    Expanded functionality of the Drum Machine Container to build out each 'Pad'

--- fcc/drumMachine/Components/DrumMachineContainer.js
+++ fcc/drumMachine/Components/DrumMachineContainer.js
@@ -1,13 +1,22 @@
+const pad = document.createElement("script")
+pad.src = `./fcc/drumMachine/Components/Pad${minStr}.js`
+document.body.appendChild(pad)
+
 class DrumContainer extends React.Component {
     constructor(props){
         super(props)
+        const kit = !this.props.kit ? ["q", "w", "e", "a", "s", "d", "z", "x", "c"] : this.props.kit
+        this.state = {
+            kit: kit.map(pad=>e(Pad, {trigger:pad, key:pad}))
+        }
+        console.log(this.state.kit)
     }
 
     render() {
         return(e(
             "div",
-            {className:"container"},
-            "test"
+            {},
+            this.state.kit
         ))
     }
 }
