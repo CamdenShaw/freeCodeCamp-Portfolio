c6da68ee809f488b724232a480285d7b3389a54f
Camden_Shaw <camden.shaw@gmail.com>
2022-02-28 22:19:39 -0800

    Built skeleton on the Pad element and learned how to use React Fragements.

---
+++ fcc/drumMachine/Components/Pad.js
@@ -0,0 +1,23 @@
+class Pad extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+    render() {
+        return(e(
+            "div",
+            {className:"pad"},
+            [
+                e(
+                    React.Fragment,
+                    {key:"text"},
+                    this.props.trigger
+                ), e(
+                    "audio",
+                    {key: "sound"},
+                    "Your browser does not support the <audio> element"
+                )
+            ]
+        ))
+    }
+}
